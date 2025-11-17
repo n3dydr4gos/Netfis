7d4f5b61e0eede504d2409084bd0cd4befc95bde
davide-its <davide.martinelli@edu-its.it>
2025-11-17 12:25:32 +0100

    Aggiunta altre informazioni i nhero section

--- src/components/FirstMovieHero.jsx
+++ src/components/FirstMovieHero.jsx
@@ -25,18 +25,22 @@ export default function FirstMovieHero({ firstMovie, firstMovieImage, firstMovie
 
             {/* Content */}
 
-      <div className="relative z-20 max-w-2xl">
-        <h2 className="text-white text-4xl lg:text-7xl font-extrabold drop-shadow-lg mb-4 text-shadow-[2px_2px_40px_rgba(0,0,0,0.5)] text-shadow-white">
-          {firstMovie?.title}
-        </h2>
-        <h3 className="text-white text-md leading-relaxed drop-shadow-md">
-          {firstMovie?.overview}
-        </h3>
-        <Button>
-          {" "}
-          <Play /> Guarda ora{" "}
-        </Button>
-      </div>
-    </div>
-  );
+            <div className="relative z-20 max-w-2xl">
+                <h2 className="text-white text-4xl lg:text-7xl font-extrabold drop-shadow-lg mb-4 text-shadow-[2px_2px_40px_rgba(0,0,0,0.5)] text-shadow-white">
+                    {firstMovie?.title}
+                </h2>
+                <h3 className="text-white text-md leading-relaxed drop-shadow-md">
+                    {firstMovie?.overview}
+                </h3>
+                <div className="flex flex-col md:flex-row md:gap-4">
+                    <Button>
+                        <Play /> Guarda ora
+                    </Button>
+                    <Button type={"secondary"}>
+                        Altre informazioni
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
 }
